7cb5239323ca232c0067be245fa419029bfe17d2
sebbbasdl <63890439+sebbbasdl@users.noreply.github.com>
2021-09-08 19:36:28 -0600

    primer commit

---
+++ Proyecto1_OLC1/src/proyecto1_olc1/Proyecto1_OLC1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_olc1;
+
+/**
+ *
+ * @author sebas
+ */
+public class Proyecto1_OLC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz ven =new Interfaz();
+        ven.setVisible(true);
+    }
+    
+}
